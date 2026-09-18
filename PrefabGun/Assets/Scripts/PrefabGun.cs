62c8e2f425f2126deb16159d30b0070a69886274
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2026-09-18 15:20:45 -0400

    Removed Mode added incriment and decriment index

--- PrefabGun/Assets/Scripts/PrefabGun.cs
+++ PrefabGun/Assets/Scripts/PrefabGun.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem;
 public class PrefabGun : MonoBehaviour
 {
     //0 is scan, 1 is create
-    public int mode = 0;
     public MeshRenderer colour;
     [SerializeField] Material green;
     [SerializeField] Material red;
@@ -24,15 +23,6 @@ public class PrefabGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        if (mode == 0)
-        {
-            colour.material = green;
-        }
-        else if (mode == 1)
-        {
-            colour.material = red;
-        }
-
         if (followMouse)
         {
             RaycastHit hit;
@@ -50,74 +40,61 @@ public class PrefabGun : MonoBehaviour
         {
             LayerMask grabObjectsLayer = LayerMask.GetMask("Prefab");
             RaycastHit hit;
-            if (mode == 0)
+            if (Physics.SphereCast(transform.position, 0.25f, transform.forward, out hit, 5f, grabObjectsLayer))
             {
-                if (Physics.SphereCast(transform.position, 0.25f, transform.forward, out hit, 5f, grabObjectsLayer))
+                if (hit.collider != null && hit.collider.gameObject.layer != 10 && hit.collider.gameObject.layer != 11)
                 {
-                    if (hit.collider != null && hit.collider.gameObject.layer != 10 && hit.collider.gameObject.layer != 11)
-                    {
-                        Debug.Log("raycast hit");
-                        savedObjects.Add(hit.collider.gameObject);
-                    }
-                }
-            }
-            else if (mode == 1)
-            {
-                if (Physics.Raycast(transform.position, transform.forward, out hit, 5f))
-                {
-                    Instantiate(savedObjects[curObjIndex], new Vector3(hit.point.x, hit.point.y + savedObjects[curObjIndex].gameObject.transform.localScale.y / 2, hit.point.z), new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
+                    Debug.Log("raycast hit");
+                    savedObjects.Add(hit.collider.gameObject);
                 }
             }
         }
     }
     public void OnRightClick(InputAction.CallbackContext context)
     {
-        if(mode == 1)
-        {
-            if (context.started)
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, transform.forward, out hit, 5f))
-                {
-                    toPlace = Instantiate(savedObjects[curObjIndex], new Vector3(hit.point.x, hit.point.y + savedObjects[curObjIndex].gameObject.transform.localScale.y / 2, hit.point.z), new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
-                    toPlace.GetComponent<MeshRenderer>().material = canBePlaced;
-                    toPlace.GetComponent<Collider>().isTrigger = true;
-                    toPlace.GetComponent<Rigidbody>().isKinematic = true;
-                    followMouse = true;
-                }
-            }
-            if (context.canceled && toPlace != null) 
-            {
-                followMouse = false;
-                Vector3 placePos = toPlace.transform.position;
-                Destroy(toPlace.gameObject);
-                toPlace = null;
-                Instantiate(savedObjects[curObjIndex], placePos, new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
-            }
-        }
-    }
-
-    public void OnSwapMode(InputAction.CallbackContext context)
-    {
         if (context.started)
         {
-            if (mode == 0)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 5f))
             {
-                mode = 1;
-            }
-            else if (mode == 1)
-            {
-                mode = 0;
+                toPlace = Instantiate(savedObjects[curObjIndex], new Vector3(hit.point.x, hit.point.y + savedObjects[curObjIndex].gameObject.transform.localScale.y / 2, hit.point.z), new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
+                toPlace.GetComponent<MeshRenderer>().material = canBePlaced;
+                toPlace.GetComponent<Collider>().isTrigger = true;
+                toPlace.GetComponent<Rigidbody>().isKinematic = true;
+                followMouse = true;
             }
         }
+        if (context.canceled && toPlace != null)
+        {
+            followMouse = false;
+            Vector3 placePos = toPlace.transform.position;
+            Destroy(toPlace.gameObject);
+            toPlace = null;
+            Instantiate(savedObjects[curObjIndex], placePos, new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
+        }
     }
-    public void OnSwapIndex(InputAction.CallbackContext context)
+    public void OnPlusIndex(InputAction.CallbackContext context)
     {
         if (context.started)
         {
             if (curObjIndex < savedObjects.Count - 1)
             {
                 curObjIndex += 1;
+            }
+            else
+            {
+                curObjIndex = 0;
+            }
+            displayIndex.text = (curObjIndex + 1).ToString();
+        }
+    }
+    public void OnMinusIndex(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (curObjIndex > 0)
+            {
+                curObjIndex -= 1;
             }
             else
             {
